217b9f747481a888a88c9678497b9f557cf3f107
Ali Imran Anjum <ali.skole@outlook.com>
2022-08-23 12:23:52 +0200

    Task 1_1_2_2 completed

--- src/Weekend_Task/Week_34/Oppgave_1.java
+++
@@ -1,7 +0,0 @@
-package Weekend_Task.Week_34;
-
-public class Oppgave_1 {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
---
+++ src/Weekend_Task/Week_34/Oppgave_1_1_2_2.java
@@ -0,0 +1,26 @@
+package Weekend_Task.Week_34;
+//Lag en min-metode på samme måte som maks-metoden i Programkode 1.1.2, dvs.
+// en metode som finner (og returnerer) posisjonen til den minste verdien i
+// en tabell.
+
+public class Oppgave_1_1_2_2 {
+
+    public static int min(int[]a){
+        int m = 0;
+
+        for(int i = 1; i < a.length;i++){
+            if(a[i] < a[m]) {
+                m=i;
+            }
+        }
+        return m;
+
+    }
+    public static void main(String[] args) {
+        int[] a= {8,4,17,10,6,20,1,11,15,3,18,9,2,7,19};
+        System.out.println(min(a));
+
+
+
+    }
+}
